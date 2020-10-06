0d8bea8230116d30e604a7d3f222dff52c20e4da
akki225 <69514242+akki225@users.noreply.github.com>
2020-10-06 15:17:21 +0530

    Add files via upload

---
+++ jenkinsdemoproject_repo/jenkinsdemoproject/src/test/java/com/dxc/jenkinsdemoproject/business/CalculatorTest.java
@@ -0,0 +1,41 @@
+package com.dxc.jenkinsdemoproject.business;
+import static org.junit.Assert.assertEquals;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest 
+{
+
+	Calculator calc=null;
+	@Before
+	public void setUp() throws Exception
+	{
+		calc=new Calculator();
+	}
+	@After
+	public void tearDown() throws Exception
+	{
+		calc=null;
+	}
+	@Test
+	public void testsum()
+	{
+		assertEquals(30,calc.sum(10, 20));
+	}
+	@Test
+	public void testSqr()
+	{
+		assertEquals(25,calc.square(5));
+	}
+	@Test
+	public void testPower()
+	{
+		assertEquals(30,calc.power(5,3));
+	}
+	@Test
+	public void testDiv()
+	{
+		assertEquals(30,calc.divide(10, 20));
+	}
+}
